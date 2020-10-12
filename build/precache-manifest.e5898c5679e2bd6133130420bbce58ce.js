d817de1f3c9c2fae105ed1cc44517eb5451aadf1
LucasK1 <l.kozlowski@mailbox.org>
2020-10-12 19:44:15 +0200

    Build

---
+++ build/precache-manifest.e5898c5679e2bd6133130420bbce58ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb9bd034ddc12de8d157c0aab3b9f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21ace098ce16122bcba",
+    "url": "/static/css/main.49cd39b2.chunk.css"
+  },
+  {
+    "revision": "91c6082057907d2c7e1b",
+    "url": "/static/js/2.ed660dc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed660dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21ace098ce16122bcba",
+    "url": "/static/js/main.8856c6be.chunk.js"
+  },
+  {
+    "revision": "5a7b70e42f3046d271df",
+    "url": "/static/js/runtime-main.4991e669.js"
+  }
+]);
