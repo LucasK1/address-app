06b623220e2759d6e5a2b088399596af5111e8f1
LucasK1 <l.kozlowski@mailbox.org>
2020-11-26 14:42:58 +0100

    Add sorting options

--- src/components/Addresses/Addresses.js
+++ src/components/Addresses/Addresses.js
@@ -9,6 +9,7 @@ import Form from 'components/Form/Form';
 
 import * as classes from './Addresses.module.css';
 import { AddressesContext } from '../../context/AddressesContext';
+import Button from 'components/UI/Button/Button';
 
 const Addresses = (props) => {
   const [loading, setLoading] = useState(false);
@@ -76,29 +77,78 @@ const Addresses = (props) => {
       });
   };
 
+  const sort = (e, keyword) => {
+    e.preventDefault();
+    setLoading(true);
+    let sorted;
+    switch (keyword) {
+      case 'byAlpha':
+        sorted = fetchedAddresses.sort((a, b) => {
+          const x = a.address.name.toLowerCase();
+          const y = b.address.name.toLowerCase();
+          if (x < y) {
+            return -1;
+          }
+          if (x > y) {
+            return 1;
+          }
+          return 0;
+        });
+        break;
+      case 'byNewest':
+        sorted = fetchedAddresses.sort(
+          (a, b) => a.address.addedOn < b.address.addedOn
+        );
+        break;
+      case 'byOldest':
+        sorted = fetchedAddresses.sort(
+          (a, b) => a.address.addedOn > b.address.addedOn
+        );
+        break;
+      default:
+        sorted = fetchedAddresses;
+        break;
+    }
+    setFetchedAddresses([...sorted]);
+    setLoading(false);
+  };
+
   return (
-    <div className={classes.Addresses}>
-      <Backdrop show={showModal} clicked={showModalHandler} />
-      <Modal show={showModal}>
-        <Form
-          address={singleAddress}
-          submitted={showModalHandler}
-          isAddressPage
-        />
-      </Modal>
-      {loading ? (
-        <Spinner />
-      ) : (
-        fetchedAddresses.map((item) => (
-          <AddressCard
-            key={item.id}
-            address={item.address}
-            clicked={() => editHandler(item.id)}
-            onDeleteClick={(e) => deleteHandler(e, item.id)}
+    <>
+      <div className={classes.Main}>
+        <Button submitted={(e) => sort(e, 'byAlpha')}>Sort Alphabetically A-Z</Button>
+        <Button submitted={(e) => sort(e, 'byNewest')}>
+          Sort Newest-Oldest
+        </Button>
+        <Button submitted={(e) => sort(e, 'byOldest')}>
+          Sort Oldest-Newest
+        </Button>
+      </div>
+      <div className={classes.Addresses}>
+        <Backdrop show={showModal} clicked={showModalHandler} />
+        <Modal show={showModal}>
+          <Form
+            address={singleAddress}
+            submitted={showModalHandler}
+            isAddressPage
           />
-        ))
-      )}
-    </div>
+        </Modal>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            {fetchedAddresses.map((item) => (
+              <AddressCard
+                key={item.id}
+                address={item.address}
+                clicked={() => editHandler(item.id)}
+                onDeleteClick={(e) => deleteHandler(e, item.id)}
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 export default Addresses;
