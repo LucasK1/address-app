0f1513fdcc3fd480c14b7bf595b7e7f445ac38de
LucasK1 <l.kozlowski@mailbox.org>
2021-03-02 22:09:35 +0100

    Remove comments

--- src/components/Addresses/AddressCard/AddressCard.js
+++ src/components/Addresses/AddressCard/AddressCard.js
@@ -3,7 +3,6 @@ import moment from 'moment';
 
 import * as classes from './AddressCard.module.css';
 import Button from 'components/UI/Button/Button';
-// import Button from 'components/UI/Button/Button';
 
 const AddressCard = ({
   address: {
@@ -20,41 +19,6 @@ const AddressCard = ({
 }) => {
   return (
     <div className={classes.AddressCard}>
-      {/* <ul>
-        <li>
-          <b>Name:</b>
-          <p>{name}</p>
-        </li>
-        <li>
-          <b>Email:</b>
-          <p>
-            <a href={`mailto:${email}`}>{email}</a>
-          </p>
-        </li>
-        <li>
-          <b>Phone number:</b>
-          <p>{phone}</p>
-        </li>
-        <li>
-          <b>Address:</b>
-          <address>
-            <p>{streetAddress}</p>
-            <p>{cityAddress}</p>
-          </address>
-        </li>
-        <li>
-          <b>Added On:</b>
-          <p>
-            {addedOn && typeof addedOn === 'string'
-              ? moment(addedOn).format('DD/MM/YYYY hh:mm')
-              : 'Unknown'}
-          </p>
-        </li>
-        <li>
-          <b>Info:</b>
-          <p>{additionalInfo}</p>
-        </li>
-      </ul> */}
       <dl>
         <dt>Name:</dt>
         <dd>{name}</dd>
